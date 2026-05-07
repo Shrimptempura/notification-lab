d2e841f9864dc973e3b515bdb8baa57adeadcb67
Shrimptempura <ongtzxowns98@gmail.com>
2026-05-07 21:53:53 +0900

    init project

---
+++ src/main/java/portfolio/notification_lab/NotificationLabApplication.java
@@ -0,0 +1,13 @@
+package portfolio.notification_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/portfolio/notification_lab/NotificationLabApplicationTests.java
@@ -0,0 +1,13 @@
+package portfolio.notification_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
